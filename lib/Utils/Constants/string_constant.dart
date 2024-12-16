cf6cc02bb870fdf9e5b43d0822c1b24b8298c8f3
suman-farmeasy <suman@farmeasy.co>
2024-12-16 10:18:05 +0530

    created custom textspan and text for constants

--- lib/Utils/Constants/string_constant.dart
+++ lib/Utils/Constants/string_constant.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class StringConstatnt{
-  static Text FARM_EASY=Text.rich(
+class StringConstatnt {
+  static Text FARM_EASY = Text.rich(
     TextSpan(
       children: [
-        TextSpan(
-          text: 'Farm',
-          style: GoogleFonts.poppins(
-            color: Color(0xFF167C0C),
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            height: 0,
-            letterSpacing: -0.24,),
-        ),
-        TextSpan(
-          text: 'Easy',
-          style: GoogleFonts.poppins(
-            color: Color(0xFF044D3A),
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            height: 0,
-            letterSpacing: -0.24,
-          ),
-        ),
+        CustomTextSpan('Farm'),
+        CustomTextSpan('Easy'),
       ],
     ),
   );
-  static Text WELCOME_TO_FARMEASY= Text(
-    '👋 Welcome to FarmEasy ',
-    style: TextStyle(
+  static Text WELCOME_TO_FARMEASY = CustomText('Welcome to FarmEasy');
+  static Text OTP_VERIFICATION = CustomText('OTP Verification');
+  static Text REGISTER_TEXT = CustomText('Please tell us a bit about you');
+
+  static String LANDOWNER = "Land Owner";
+  static String FARMER = "Farmer";
+  static String AGRI_PROVIDER = "Agri Service Provider";
+  static String GOOGLE_PLACES_API = "AIzaSyBnqoT5GnShJwl5n-76kBGNE14VAazAZ9k";
+}
+
+// ignore: non_constant_identifier_names
+TextSpan CustomTextSpan(String title) {
+  return TextSpan(
+    text: title,
+    style: GoogleFonts.poppins(
+      color: const Color(0xFF044D3A),
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      height: 0,
+      letterSpacing: -0.24,
+    ),
+  );
+}
+
+// ignore: non_constant_identifier_names
+Text CustomText(String title) {
+  return Text(
+    title,
+    style: const TextStyle(
       color: Color(0xFF483C32),
       fontSize: 20,
       fontFamily: 'Poppins',
@@ -38,30 +47,4 @@ class StringConstatnt{
       letterSpacing: -0.20,
     ),
   );
-  static Text OTP_VERIFICATION= Text(
-    'OTP Verification ',
-    style: TextStyle(
-      color: Color(0xFF483C32),
-      fontSize: 20,
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w600,
-      height: 0,
-      letterSpacing: -0.20,
-    ),
-  );
-  static Text REGISTER_TEXT= Text(
-    'Please tell us a bit about you ',
-    style: TextStyle(
-      color: Color(0xFF483C32),
-      fontSize: 20,
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w600,
-      height: 0,
-      letterSpacing: -0.20,
-    ),
-  );
-  static String LANDOWNER= "Land Owner";
-  static String FARMER= "Farmer";
-  static String AGRI_PROVIDER= "Agri Service Provider";
-  static String GOOGLE_PLACES_API= "AIzaSyBnqoT5GnShJwl5n-76kBGNE14VAazAZ9k";
 }
